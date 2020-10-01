8770920ff023ba9dfb041a7a7e5fa89a2bac5078
Dmitrii Matveev <matveyeff@mail.ru>
2020-10-01 16:35:04 +0300

    new banner is added

--- src/js/main.js
+++ src/js/main.js
@@ -36,19 +36,6 @@ $(window).on('load', function(){
 	});
 });
 
-document.addEventListener('appReady.EappsInstagramFeed', function() {
-	$('.my-instashow').eappsInstagramFeed({
-		api: '/instashow/api/index.php',
-		source: '@beauty_force.ru',
-		width: 'auto',
-		layout: 'grid',
-		columns: 4,
-		rows: 2,
-		lang: 'ru',
-		colorGridLoadMoreButton: 'rgb(0, 53, 51)'
-	});
-});
-
 baguetteBox.run('.baguetteBoxThree', {
 	animation: 'fadeIn',
 	noScrollbars: true,
